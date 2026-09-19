3e6c6c52cd6178ace762c84e4c423d1c429b78cb
AAAAAAANK <leonschmitt110@gmail.com>
2026-09-19 14:31:39 +0200

    Add update handling routes for script uploads

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 app = Flask(__name__)
 
 messages = []
-
+update = []
 @app.route("/")
 def home():
     return "Termux Chat Server läuft!"
@@ -17,6 +17,18 @@ def send():
 @app.route("/messages")
 def get_messages():
     return {"messages": messages}
+
+@app.route("/sendupdate", methods=["POST"])
+def uploadupdate():
+    update.clear()
+    script = request.json["script"]
+    update.append(script)
+    return {"status": "uploaded"}
+
+
+@app.route("/getupdate")
+def download_update():
+    return {"update": "update"}
 
 @app.route("/clear", methods=["POST"])
 def clear():
